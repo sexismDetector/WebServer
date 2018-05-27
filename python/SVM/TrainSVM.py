1436542b6b90b02869f011a40dd0dddfd717a53f
victor <osiajod@gmail.com>
2018-05-27 16:15:44 +0900

    awesome SVMs

--- python/SVM/TrainSVM.py
+++ python/SVM/TrainSVM.py
@@ -10,22 +10,41 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
 #read SVM.csv from /res directory.
-svm_table = pd.read_csv("../../res/svm.csv") #x1~x6 data is obtained as a result.
+svm_table = pd.read_csv("svm.csv") #x1~x6 data is obtained as a result.
 
 
 # row_count = svm_table.shape[0]   # shape returns a tuple of (rows, cols)  ==> shape[0] means num of rows
 
-x7 = []
+# print(svm_table)
+# x7 = pd.DataFrame(columns=["neg", "neu", "pos", "compound"])
+# print(svm_table.isnull().sum().sum())
+x7_df = pd.DataFrame(columns=["neg", "neu", "pos", "compound"])
+
 #compute x7: sentimental value
 analyzer = SentimentIntensityAnalyzer()
 for tweet in svm_table["text"]:  # run vaderSentiment on all tweets
-    vs = analyzer.polarity_scores(tweet)
-    x7.append(vs)
+    senti = analyzer.polarity_scores(tweet)
+    # print(neg)
+    neg = senti['neg']
+    neu = senti['neu']
+    pos = senti['pos']
+    compound = senti['compound']
+    new_series = pd.Series([neg, neu, pos, compound], index=["neg", "neu","pos","compound"])
+    # print("new:")
+    # print(new_series)
+    # dict1.update({"neg" : neg, "pos" : pos, "compound" : compound})
+    x7_df = x7_df.append(new_series, ignore_index=True)   # CAUTION !!! pandas' df.append is not in-place appending, so has to return by = operator
+    # print(x7_df)
 
+# x7_df = pd.DataFrame(x7)
 
+# x7_series = pd.Series(x7)
+
+# print(x7_df)
+# print(x7_df.isnull().values.any())
 #use numpy to bond x7 column to the pd
-result_table = pd.concat([svm_table, x7], axis= 1, sort=False) # keep the order as is
-
+result_table = pd.concat([svm_table, x7_df], axis= 1) # keep the order as is
+print("here")
 # SVM_models = []
 
 #run SVM on the pd
@@ -36,14 +55,23 @@ svm_fit_poly = SVC(kernel='poly', probability=True)
 svm_fit_sig= SVC(kernel='sigmoid', probability=True)
 # text , x1 = urban , x2 = oxford , x3 = follower , x4 = favorite , x5 = friend , x6 = sexist words/ length ratio
 # 'some_sexist_word'   ==> urbandictionary def with sexual connotations 7 ==> 7 * #occurence in tweet 6 = 7 * 6 =42
-X_df = result_table.loc[:,['x1','x2','x3','x4','x5','x6','x7']] # take everything from x1 ~x7 as our X, and fit it to 'label'
+# X_df = result_table.loc[:,['urban_score','oxford_score','follower_score','favorite_score','friend_score','sex_words_ratio','x7']] # take everything from x1 ~x7 as our X, and fit it to 'label'
+# X_df = result_table[list(result_table.columns[1:6]result_table.columns[7]] # take everything from x1 ~x7 as our X, and fit it to 'label'
+# print(result_table)
+cols2exclude = [0,7]
+# X_df = result_table.iloc[:, result_table.columns.difference(result_table.columns[cols2exclude])]
+X_df = result_table[['urban_score','oxford_score','follower_score','favorite_score','friend_score','sex_words_ratio','neg','neu', 'pos', 'compound']]
+print(X_df)
+
 # Note that  .loc[: , 'x1':'x7'] may not work because x7 was concatenated after 'label' col of the original .csv
-Y_df = result_table[:,'label']
+Y_df = result_table['label']
 
 # the reason I specified the random_state int is because I want to split of the df to be reproducible.
 # For future reference, I am saving the value of random_state_seed along with the accuracy info in [model].txt.
 random_state_seed = np.random.random_integers(0, 100) # produce random integer [0, 100]
 X_df_train, X_df_test, Y_df_train, Y_df_test = train_test_split(X_df, Y_df, test_size=0.2, random_state=random_state_seed)
+
+# print(X_df.isnull().values.any())
 
 svm_rbf = svm_fit_rbf.fit(X_df_train, Y_df_train)
 svm_lnr = svm_fit_lnr.fit(X_df_train, Y_df_train)
