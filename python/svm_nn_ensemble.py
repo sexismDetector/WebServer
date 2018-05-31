cb2311a8a7d921814b7379ffd5cacb5bfcd7d443
acevedo-oscar <oscar.acevedo@comunidad.unam.mx>
2018-05-31 18:46:31 +0900

    added ensemble code

---
+++ python/svm_nn_ensemble.py
@@ -0,0 +1,54 @@
+"""
+Ensemble using SVM and NN for sexist commennts binary classification 
+
+""" 
+# For reading the json
+import os
+import sys, json
+
+# For using the models
+from sklearn.model_selection import *
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.ensemble import  VotingClassifier
+import pandas
+import pickle
+
+from  nn_model import nn_ensemble_model
+
+
+while True:
+
+    #reading the jsons
+    tweet = sys.stdin.readline()
+    user = sys.stdin.readline()
+    first_json = json.loads(tweet)
+    second_json = json.loads(user) 
+    
+    ### BEGINNING OF ESEMBLE CODE ###
+
+    script_path = os.path.dirname(os.path.abspath(__file__))
+
+    svm_path = script_path+"/SVM/svm_rbf.sav"
+
+    txtSVM loaded_model = pickle.load(open(svm_path, 'rb'))
+
+
+    #Creating sci-kit learn object for classifier voting
+    sk_nn_classifier = KerasClassifier(build_fn = nn_ensemble_model)
+
+    estimators = []
+    model1 = txtSVM
+    estimators.append(('svm', model1))
+    model2 = sk_nn_classifier
+    estimators.append(('nn', model2))
+
+    # create the Voting Classifier ensemble model
+    ensemble = VotingClassifier(estimators)
+
+    print(ensemble)
+
+
+
+   
+    #### END OF ENSEMBLE CODE ####
+    sys.stdout.flush()
